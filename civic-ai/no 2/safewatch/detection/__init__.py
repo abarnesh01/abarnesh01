62ccb2276c51ef3cc5430d3f5cca5dcbafd18142
abarnesh01 <abarnesh772@gmail.com>
2026-05-13 21:09:14 +0530

    feat: export detection components in __init__.py

--- civic-ai/no 2/safewatch/detection/__init__.py
+++ civic-ai/no 2/safewatch/detection/__init__.py
@@ -1,8 +1,4 @@
-"""SafeWatch Detection Package."""
-
-from detection.person_detector import PersonDetector
-from detection.pose_estimator import PoseEstimator
-from detection.optical_flow import OpticalFlowAnalyzer
-from detection.zone_manager import ZoneManager
-
-__all__ = ["PersonDetector", "PoseEstimator", "OpticalFlowAnalyzer", "ZoneManager"]
+from .person_detector import PersonDetector
+from .pose_estimator import PoseEstimator
+from .optical_flow import OpticalFlowAnalyzer
+from .zone_manager import ZoneManager
